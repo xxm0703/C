dfcf0238e195281eb60128edc79f8878196e4b24
xxm0703 <xxm0703@abv.bg>
2017-11-08 19:13:49 +0200

    Infinity Line

--- Struct/inf.c
+++ Struct/inf.c
@@ -1,7 +1,5 @@
 #include"typedef.h"
 #include<stdio.h>
-//3 = a  
-//9-4a
 
 int main(){
 	point_t p1, p2;
@@ -9,12 +7,10 @@ int main(){
 	p2.y = 3;
 	p1.x = 4;
 	p1.y = 9;
-	float a = 1,b;
-	for(int i=0;i<100;++i){
-		b = p1.y-p1.x*a;
-		a = (p2.y-b)/p2.x;
-	}
-	printf("f(x) = %f*x + %f", a, b);
+	float a, b;
+	a = (p2.y - p1.y) / (p2.x - p1.x);
+	b = p1.y-a*p1.x;
+	printf("f(x) = %.2f*x + %.2f", a, b);
 
 	return 0;
 }
--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-a=`find $1 -mindepth 1 -not -path '*/\.*' -type f \( ! -iname "*.out" \)`
+a=`find $1 -mindepth 1 -not -path '*/\.*' -type f \( -iname "*.c" \)`
 for f in $a
 do
 name="${f%%.*}"
 gcc -pedantic-errors -std=c11 -Wall -o $name.out $f -lm
-echo "Done compilation of ${f%%*/}"
+echo "Done compilation of ${name}"
 done
