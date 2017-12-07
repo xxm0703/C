52da96df5908e0f4f9cb4fb4a2f11092311634ca
xxm0703 <xxm0703@abv.bg>
2017-12-07 17:40:28 +0200

    Finde And Replace

--- Pointers/min_max.c
+++ Pointers/min_max.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 
+void print(int *arr, int size);
 void min_max(int *arr, int size, int *min, int *max);
+void find_replace(int *arr, int size, int find, int replace);
 int main(){
 	int arr[] = {13,2,30,4,1,5,6};
-	int *malko = arr;
-	int *golqmo = arr;
-	min_max(arr,7,malko, golqmo);
-	printf("MIN: %p - %d", (void*)malko, *malko);
-	printf("MAX: %p - %d", (void*)golqmo, *golqmo);	
+	int malko, golqmo;
+	min_max(arr,7,&malko, &golqmo);
+	print(arr, 7);
+	find_replace(arr, 7, malko, golqmo);
+	print(arr, 7);
+	printf("MIN: %p - %d\n", (void*)(&malko), malko);
+	printf("MAX: %p - %d\n", (void*)(&golqmo), golqmo);	
 	return 0;
 }
 
+void print(int *arr, int size){
+	for(int i = 0; i < size; ++i){
+		printf("%d ", arr[i]);
+	}	
+	printf("\n");
+}
+
+void find_replace(int *arr, int size, int find, int replace){
+	for(int i = 0; i < size; ++i){
+		if(arr[i] == find)arr[i] = replace;
+	} 
+}
 void min_max(int *arr, int size, int *min, int *max){
 	for(int i = 0; i < size; ++i){
-		if(*min > arr[i]) min = arr+i;
-		else if(*max < arr[i]) max = arr+i;
+		if(*min > arr[i]) *min = arr[i];
+		else if(*max < arr[i]) *max = arr[i];
 	}
 }
